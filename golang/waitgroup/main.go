375a71832bd3a4a9f8d399b9c8bc37a849cd3cd5
Ismal zikri <zikriismal42@gmail.com>
2024-06-15 11:00:47 +0700

    add new topic WaitGroup

---
+++ golang/waitgroup/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+func doPrint(wg *sync.WaitGroup, message string) {
+	defer wg.Done()
+	fmt.Println(message)
+}
+
+func main() {
+	runtime.GOMAXPROCS(2)
+
+	var wg sync.WaitGroup
+
+	for i := 0; i < 5; i++ {
+		var data = fmt.Sprintf("data %d", i)
+
+		wg.Add(1)
+		go doPrint(&wg, data)
+	}
+
+	wg.Wait()
+}
