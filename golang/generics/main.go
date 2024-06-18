bb629733811f1fef2224087ad6ab0b0aeabaae24
Ismal zikri <zikriismal42@gmail.com>
2024-06-18 14:10:21 +0700

    Struct Generic

--- golang/generics/main.go
+++ golang/generics/main.go
@@ -18,27 +18,43 @@ import "fmt"
 // 	return total
 // }
 
-func SumNumbers1(m map[string]int64) int64 {
-	var s int64
-	for _, v := range m {
-		s += v
-	}
-	return s
+// func SumNumbers1(m map[string]int64) int64 {
+// 	var s int64
+// 	for _, v := range m {
+// 		s += v
+// 	}
+// 	return s
+// }
+
+// func SumNumbers2[K comparable, V int64 | float64](m map[K]V) V {
+// 	var s V
+// 	for _, v := range m {
+// 		s += v
+// 	}
+// 	return s
+// }
+
+type UserModel[T int | float64] struct {
+	Name   string
+	Scores []T
 }
 
-func SumNumbers2[K comparable, V int64 | float64](m map[K]V) V {
-	var s V
-	for _, v := range m {
-		s += v
-	}
-	return s
+func (m *UserModel[int]) SetScoresA(scores []int) {
+	m.Scores = scores
+}
+
+func (m *UserModel[float64]) SetScoresB(scores []float64) {
+	m.Scores = scores
 }
 
 func main() {
-	ints := map[string]int64{"first": 34, "second": 12}
-	floats := map[string]float64{"first": 35.98, "second": 26.99}
+	var m1 UserModel[int]
+	m1.Name = "Noval"
+	m1.Scores = []int{1, 2, 3}
+	fmt.Println("scores:", m1.Scores)
 
-	fmt.Printf("Generic Sums with Constraint: %v and %v\n",
-		SumNumbers2(ints),
-		SumNumbers2(floats))
+	var m2 UserModel[float64]
+	m2.Name = "Noval"
+	m2.SetScoresB([]float64{10, 11})
+	fmt.Println("scores:", m2.Scores)
 }
