4a6eb570d622511b1ebbe13007ac87a4e9677d20
Ismal zikri <zikriismal42@gmail.com>
2024-06-18 14:02:21 +0700

    keyword comparable

--- golang/generics/main.go
+++ golang/generics/main.go
@@ -10,21 +10,35 @@ import "fmt"
 // 	return total
 // }
 
-func Sum[V int | float32 | float64](numbers []V) V {
-	var total V
-	for _, e := range numbers {
-		total += e
+// func Sum[V int | float32 | float64](numbers []V) V {
+// 	var total V
+// 	for _, e := range numbers {
+// 		total += e
+// 	}
+// 	return total
+// }
+
+func SumNumbers1(m map[string]int64) int64 {
+	var s int64
+	for _, v := range m {
+		s += v
 	}
-	return total
+	return s
+}
+
+func SumNumbers2[K comparable, V int64 | float64](m map[K]V) V {
+	var s V
+	for _, v := range m {
+		s += v
+	}
+	return s
 }
 
 func main() {
-	total1 := Sum([]int{1, 2, 3, 4, 5})
-	fmt.Println("total:", total1)
+	ints := map[string]int64{"first": 34, "second": 12}
+	floats := map[string]float64{"first": 35.98, "second": 26.99}
 
-	total2 := Sum([]float32{2.5, 7.2})
-	fmt.Println("total:", total2)
-
-	total3 := Sum([]float64{1.23, 6.33, 12.6})
-	fmt.Println("total:", total3)
+	fmt.Printf("Generic Sums with Constraint: %v and %v\n",
+		SumNumbers2(ints),
+		SumNumbers2(floats))
 }
