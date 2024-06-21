20df88308e99ad9b259870ad575e68e92246a217
Ismal zikri <zikriismal42@gmail.com>
2024-06-21 10:00:20 +0700

    update human readable error handling

--- golang/http-error-handling-validation/main.go
+++ golang/http-error-handling-validation/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/go-playground/validator/v10"
@@ -25,14 +26,43 @@ func main() {
 	e := echo.New()
 	e.Validator = &CustomValidator{validator: validator.New()}
 
+	// e.HTTPErrorHandler = func(err error, c echo.Context) {
+	// 	report, ok := err.(*echo.HTTPError)
+	// 	if !ok {
+	// 		report = echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+	// 	}
+
+	// 	c.Logger().Error(report)
+	// 	c.JSON(report.Code, report)
+	// }
+
 	e.HTTPErrorHandler = func(err error, c echo.Context) {
 		report, ok := err.(*echo.HTTPError)
+
 		if !ok {
 			report = echo.NewHTTPError(http.StatusInternalServerError, err.Error())
 		}
 
+		if castedObject, ok := err.(validator.ValidationErrors); ok {
+			for _, err := range castedObject {
+				switch err.Tag() {
+				case "required":
+					report.Message = fmt.Sprintf("%s is required", err.Field())
+				case "email":
+					report.Message = fmt.Sprintf("%s is not valid email", err.Field())
+				case "gte":
+					report.Message = fmt.Sprintf("%s value must be greater than %s", err.Field(), err.Param())
+				case "lte":
+					report.Message = fmt.Sprintf("%s value must be lower than %s", err.Field(), err.Param())
+				}
+
+				break
+			}
+		}
+
 		c.Logger().Error(report)
 		c.JSON(report.Code, report)
+
 	}
 
 	e.POST("/users", func(c echo.Context) error {
