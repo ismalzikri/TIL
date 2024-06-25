f8019eb581a7f4dbd9f6861c6c0d81b09ccdc0a2
Ismal zikri <zikriismal42@gmail.com>
2024-06-25 10:47:32 +0700

    add new topic convert html to pdf

---
+++ golang/convert-html-to-pdf/go.mod
@@ -0,0 +1,5 @@
+module convert-html-to-pdf
+
+go 1.22.4
+
+require github.com/SebastiaanKlippert/go-wkhtmltopdf v1.9.2
---
+++ golang/convert-html-to-pdf/html-plain/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/SebastiaanKlippert/go-wkhtmltopdf"
+)
+
+func main() {
+	pdfg, err := wkhtmltopdf.NewPDFGenerator()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	f, err := os.Open("./input.html")
+	if f != nil {
+		defer f.Close()
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	pdfg.AddPage(wkhtmltopdf.NewPageReader(f))
+
+	pdfg.Orientation.Set(wkhtmltopdf.OrientationPortrait)
+	pdfg.Dpi.Set(300)
+
+	err = pdfg.Create()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = pdfg.WriteFile("./output.pdf")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Done")
+}
